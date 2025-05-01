d9dbf44342f325c56969156160597e1738212be7
sidgaikwad <gaikwadsiddharth039@gmail.com>
2025-05-01 23:58:36 +0530

    changes for the datatypes in the rust ans variable shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+
+    datatypes();
 }
+
+fn datatypes() {
+    let x: i32 = 500;
+    println!("the value of x is {}", x);
+
+    let x: &str = "this is false";
+
+    println!("the value of x is {}", x)
+
+}
+
+
